5306f42d34895cacb0912d259cb09f6df5af222c
Felipe Paz <xxfelipepaz@gmail.com>
2022-03-23 21:58:32 -0300

    criado script para renomear os arquivos

--- main.go
+++ main.go
@@ -12,7 +12,9 @@ func main() {
 
 	err := filepath.Walk("./", func(path string, info os.FileInfo, err error) error {
 
-		if !strings.HasPrefix(path, ".") && !strings.HasPrefix(path, "exercicio") && !strings.HasPrefix(path, "main.go") {
+		if !strings.HasPrefix(path, "main.go") &&
+			!strings.HasPrefix(path, "exercicios") &&
+			strings.Contains(path, ".go") {
 			folders = append(folders, path)
 		}
 
@@ -23,5 +25,18 @@ func main() {
 		panic(err)
 	}
 
-	fmt.Println(folders)
+	for _, folder := range folders {
+		originalPath := folder
+
+		f := strings.Split(folder, "/")
+		newPath := f[0] + "/main.go"
+
+		err := os.Rename(originalPath, newPath)
+
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Println("Concluído!!!")
 }
